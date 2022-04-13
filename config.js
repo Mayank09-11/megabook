31f4c290bb2fd4ed232b072621425791182825df
Mayank09-11 <73886619+Mayank09-11@users.noreply.github.com>
2022-04-13 16:54:52 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAdNZxM9WQriHrmClDCnPW7FHrUDSaV7AA",
+    authDomain: "mega-book-5b695.firebaseapp.com",
+    projectId: "mega-book-5b695",
+    storageBucket: "mega-book-5b695.appspot.com",
+    messagingSenderId: "190133520204",
+    appId: "1:190133520204:web:1786303418aff204aa71e9",
+    measurementId: "G-XWHERSY47K"
+  };
